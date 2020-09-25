2f344681c9cad75c7f54d7fbbbd780a6952fd668
wang_cong0918 <1048713621@qq.com>
2020-09-25 15:03:03 +0800

    JWT认证

---
+++ utils/jwt/jwt.go
@@ -0,0 +1,78 @@
+package jwt
+import (
+	"JuFeng/sunrise/log"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"reflect"
+	"time"
+	"errors"
+)
+
+const (
+	SecretKey = "sunrise"
+)
+type User struct {
+	UserID  string `json:"omitempty"`
+	Phone string  `json:"omitempty"`
+	OpenID  string  `json:"omitempty"`
+	UserName    string `json:"omitempty"`
+	Address string  `json:"omitempty"`
+	Remark   string `json:"omitempty"`
+}
+
+//自定义payload结构体
+type userStdClaims struct {
+	jwt.StandardClaims
+	*User
+}
+
+func JwtGenerateToken(u *User,d time.Duration) (string, error) {
+	expireTime := time.Now().Add(d)
+	stdClaims := jwt.StandardClaims{
+		ExpiresAt: expireTime.Unix(),
+		IssuedAt:  time.Now().Unix(),
+		Id:        fmt.Sprintf("%d", u.UserID),
+		Issuer:    SecretKey,
+	}
+
+	uClaims := userStdClaims{
+		StandardClaims: stdClaims,
+		User:           u,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, uClaims)
+	tokenString, err := token.SignedString([]byte(SecretKey))
+	if err != nil {
+		log.Logger.Info("config is wrong, can not generate jwt------------->", err)
+	}
+	return tokenString,err
+}
+func GetIdFromClaims(key string, claims jwt.Claims) string {
+	v := reflect.ValueOf(claims)
+	if v.Kind() == reflect.Map {
+		for _, k := range v.MapKeys() {
+			value := v.MapIndex(k)
+			if fmt.Sprintf("%s", k.Interface()) == key {
+				return fmt.Sprintf("%v", value.Interface())
+			}
+		}
+	}
+	return ""
+}
+//JwtParseUser 解析payload的内容,得到用户信息
+//gin-middleware 会使用这个方法
+func JwtParseUser(tokenString string) (*User, error) {
+	if tokenString == "" {
+		return nil, errors.New("no token is found in Authorization Bearer")
+	}
+	claims := userStdClaims{}
+	_, err := jwt.ParseWithClaims(tokenString, &claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(SecretKey), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return claims.User, err
+}
